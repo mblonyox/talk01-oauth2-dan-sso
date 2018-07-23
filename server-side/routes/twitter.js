99d064f73864942561eba2b3b72b25ddde66cfb7
Sukirno <mblonyox@yahoo.co.id>
2018-07-23 17:20:23 +0700

    Senin sore.

--- server-side/routes/twitter.js
+++ server-side/routes/twitter.js
@@ -4,31 +4,46 @@ const twitter = require('../services/twitter')
 const router = new Router()
 
 router.get('/', (req, res, next) => {
-  if (!req.session.twitterScreenName) return res.redirect('/twitter/connect')
-  res.render('welcome', {name: req.session.twitterScreenName})
+  if (!req.session.twitter || !req.session.twitter.oauthAccessToken) return res.redirect('/twitter/connect')
+  twitter.get(
+    'https://api.twitter.com/1.1/account/verify_credentials.json',
+    req.session.twitter.oauthAccessToken,
+    req.session.twitter.oauthAccessTokenSecret,
+    (error, data) => {
+      if (error) return next(new Error('Error getting account info.'))
+      data = JSON.parse(data)
+      res.render('twitter', {data})
+    }
+  )
 })
 
 router.get('/connect', (req, res, next) => {
   twitter.getOAuthRequestToken((error, token, secret, result) => {
     if (error) return next(new Error('Error getting OAuth request token.'))
-    req.session.oauthRequestToken = token
-    req.session.oauthRequestTokenSecret = secret
+    req.session.twitter = {
+      oauthRequestToken: token,
+      oauthRequestTokenSecret: secret
+    }
     return res.redirect("https://twitter.com/oauth/authenticate?oauth_token="+token)
   })
 })
 
 router.get('/callback', (req, res, next) => {
-  const requestToken = req.session.oauthRequestToken
-  const requestSecret = req.session.oauthRequestTokenSecret
+  const { oauthRequestToken, oauthRequestTokenSecret } = req.session.twitter
   const verifier = req.query.oauth_verifier
-  twitter.getOAuthAccessToken(requestToken, requestSecret, verifier, (error, token, secret, result) => {
+  twitter.getOAuthAccessToken(oauthRequestToken, oauthRequestTokenSecret, verifier, (error, token, secret, result) => {
     if (error) return next(new Error('Error getting OAuth access token.'))
-    req.session.oauthAccessToken = token
-    req.session.oauthAccessTokenSecret = secret
-    req.session.twiterUserId = result.user_id
-    req.session.twitterScreenName = result.screen_name
+    req.session.twitter.oauthAccessToken = token
+    req.session.twitter.oauthAccessTokenSecret = secret
+    req.session.twitter.userId = result.user_id
+    req.session.twitter.screenName = result.screen_name
     return res.redirect('/twitter')
   })
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.twitter = undefined
+  return res.redirect('/')
 })
 
 module.exports = router
